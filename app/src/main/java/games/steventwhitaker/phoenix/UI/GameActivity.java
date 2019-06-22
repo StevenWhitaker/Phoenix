d6d1b2078daaf466486bd35af036a1e65ba71dcf
StevenWhitaker <steventwhitaker@gmail.com>
2019-06-22 14:11:04 -0400

    Converted to androidx. Now everything works as it did before, so now I can move forward!

--- app/src/main/java/games/steventwhitaker/phoenix/UI/GameActivity.java
+++ app/src/main/java/games/steventwhitaker/phoenix/UI/GameActivity.java
@@ -2,7 +2,7 @@ package games.steventwhitaker.phoenix.UI;
 
 import android.content.Context;
 import android.content.Intent;
-import android.support.v4.app.Fragment;
+import androidx.fragment.app.Fragment;
 
 public class GameActivity extends SingleFragmentActivity
 {
